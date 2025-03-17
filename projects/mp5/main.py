3081d17bc58982fc7572b9e15371313593539e1f
Chen Chen <cchen744@wisc.edu>
2025-03-17 01:04:35 +0000

    Mar. 16

---
+++ projects/mp5/main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from flask import Flask, request, jsonify,Response
+import time
+
+# TODO: Add a comment about your data source
+#My dataset was collected from 'https://quickstats.nass.usda.gov/'
+
+app = Flask(__name__)
+df = pd.read_csv("main.csv")
+last_visit = 0
+
+@app.route('/')
+def home():
+    with open("index.html") as f:
+        html = f.read()
+
+    return html
+
+@app.route('/browser')
+def show(): 
+    return df.to_html(index=False)
+
+@app.route('/browser.json')
+def tojson():
+    global last_visit
+    print('IP address:',request.remote_addr)
+    if time.time() - last_visit>60:
+        last_visit = time.time()
+        df_json = df.to_dict(orient="records")  # Convert DataFrame to list of dictionaries
+        return jsonify(df_json)  # Return JSON response
+    else:
+        return Response("Please do not access this page so frequently", status=429,headers = {"Retry-After":60,"key":"value"})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, threaded=False) # don't change this line!
+
+# NOTE: app.run never returns (it runs for ever, unless you kill the process)
+# Thus, don't define any functions after the app.run call, because it will
+# never get that far.
