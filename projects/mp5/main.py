fa1ed96b5b46ac52c96ed5cd7a75b3c8e8c4511c
Chen Chen <cchen744@wisc.edu>
2025-03-17 21:17:32 +0000

    Mar 17

--- projects/mp5/main.py
+++ projects/mp5/main.py
@@ -8,6 +8,8 @@ import time
 app = Flask(__name__)
 df = pd.read_csv("main.csv")
 last_visit = 0
+ip_addrs = []
+
 
 @app.route('/')
 def home():
@@ -23,14 +25,25 @@ def show():
 @app.route('/browser.json')
 def tojson():
     global last_visit
-    print('IP address:',request.remote_addr)
+    ip_addr = request.remote_addr
+    ip_addrs.append(ip_addr)
+    print('IP address:',ip_addr)
     if time.time() - last_visit>60:
         last_visit = time.time()
         df_json = df.to_dict(orient="records")  # Convert DataFrame to list of dictionaries
         return jsonify(df_json)  # Return JSON response
     else:
         return Response("Please do not access this page so frequently", status=429,headers = {"Retry-After":60,"key":"value"})
+    
+    
+@app.route('/visitors.json')
+#Now add a resource at `http://your-ip:5000/visitors.json` that returns a list of the IP addresses that have visited your `browse.json` resource.
+def visitors():
+    return list(set(ip_addrs))
 
+@app.route('/donate')
+def donation():
+    return "Please fund me!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, threaded=False) # don't change this line!
